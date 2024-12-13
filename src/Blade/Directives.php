2ab6a2dc243616192fb18d69c3e472138d350d1a
BespredeL <hello@bespredel.name>
2024-12-13 16:11:10 +0300

    Feature Subresource Integrity

--- src/Blade/Directives.php
+++ src/Blade/Directives.php
@@ -25,9 +25,20 @@ class Directives
             $escapedKey = addslashes($publicKey);
             $translations = json_encode(trans('encryption_form'));
 
+            // Generate SRI hashes dynamically
+            $jsencryptPath = public_path('vendor/encryption-form/js/jsencrypt.min.js');
+            $formEncryptPath = public_path('vendor/encryption-form/js/form-encrypt.js');
+
+            if (!file_exists($jsencryptPath) || !file_exists($formEncryptPath)) {
+                throw new \Exception('Required JavaScript files are missing.');
+            }
+
+            $jsencryptSri = base64_encode(hash_file('sha384', $jsencryptPath, true));
+            $formEncryptSri = base64_encode(hash_file('sha384', $formEncryptPath, true));
+
             return <<<HTML
-<script src="/vendor/encryption-form/js/jsencrypt.min.js"></script>
-<script src="/vendor/encryption-form/js/form-encrypt.js"></script>
+<script src="/vendor/encryption-form/js/jsencrypt.min.js" integrity="sha384-{$jsencryptSri}" crossorigin="anonymous"></script>
+<script src="/vendor/encryption-form/js/form-encrypt.js" integrity="sha384-{$formEncryptSri}" crossorigin="anonymous"></script>
 <script>
     window.ENCRYPTION_FORM_PUBLIC_KEY = `{$escapedKey}`;
     window.ENCRYPTION_FORM_LANG = {$translations};
