e5a7d689611e0fdb89b5b7951e6c1eeffbdb1486
Alexander BespredeL Kireev <admin@bespredel.name>
2024-12-13 12:24:40 +0300

    Publishing a package

---
+++ src/Blade/Directives.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Bespredel\EncryptionForm\Blade;
+
+use Illuminate\Support\Facades\Blade;
+
+class Directives
+{
+    /**
+     * Registering Blade Directives
+     *
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public static function register(): void
+    {
+        Blade::directive('encryptFormScripts', function () {
+            $publicKey = config('encryption_form.public_key');
+
+            if (!$publicKey) {
+                throw new \Exception('Public key for encryption is not set in the configuration.');
+            }
+
+            $escapedKey = addslashes($publicKey);
+
+            return <<<HTML
+<script src="/vendor/encryption-form/js/jsencrypt.min.js"></script>
+<script src="/vendor/encryption-form/js/form-encrypt.js"></script>
+<script>
+    window.ENCRYPTION_FORM_PUBLIC_KEY = `{$escapedKey}`;
+</script>
+HTML;
+        });
+    }
+}
---
+++ src/EncryptionFormServiceProvider.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Bespredel\EncryptionForm;
+
+use Bespredel\EncryptionForm\Blade\Directives;
+use Bespredel\EncryptionForm\Console\Commands\GenerateEncryptionKeys;
+use Illuminate\Routing\Router;
+use Illuminate\Support\ServiceProvider;
+
+class EncryptionFormServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @param Router $router
+     *
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function boot(Router $router): void
+    {
+        $langPath = 'vendor/encryption-form';
+        $langPath = (function_exists('lang_path'))
+            ? lang_path($langPath)
+            : resource_path('lang/' . $langPath);
+
+        $this->publishes([
+            __DIR__ . '/../config/encryption_form.php' => config_path('encryption_form.php'),
+            __DIR__ . '/../resources/js'               => public_path('vendor/encryption-form/js'),
+            __DIR__ . '/../resources/lang'             => $langPath,
+        ], 'encryption-form');
+
+        $this->registerMiddleware($router);
+        $this->registerCommands();
+
+        Directives::register();
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/encryption_form.php',
+            'encryption_form'
+        );
+    }
+
+    /**
+     * Register middleware.
+     *
+     * @param Router $router
+     *
+     * @return void
+     */
+    public function registerMiddleware(Router $router): void
+    {
+        $router->aliasMiddleware('encryption-form', 'Bespredel\EncryptionForm\Middleware\EncryptionForm');
+    }
+
+    /**
+     * Register the commands.
+     *
+     * @return void
+     */
+    public function registerCommands(): void
+    {
+        $this->commands([
+            GenerateEncryptionKeys::class,
+        ]);
+
+        if (config('encryption_form.key_rotation.enabled')) {
+            $this->app->booted(function () {
+                app(Schedule::class)
+                    ->command('encryption-form:generate-keys')
+                    ->cron(config('encryption_form.key_rotation.cron_expression'));
+            });
+        }
+    }
+}
