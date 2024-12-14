234fedec4de9e4ea5bf840a935ca0a9d9088be55
Alexander BespredeL Kireev <admin@bespredel.name>
2024-12-14 12:39:37 +0300

    Refactor

--- resources/lang/en/encryption_form.php
+++ resources/lang/en/encryption_form.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'Encryption is not available. Do you want to submit the form without encryption?' => 'Encryption is not available. Do you want to submit the form without encryption?',
+    'Encryption is not available. Do you want to submit the form without encryption?' => 'Form encryption is not available. Submit a form without encryption?',
     'Form submission canceled by user.'                                               => 'Form submission canceled by user.',
     'Form encrypted successfully.'                                                    => 'Form encrypted successfully.',
     'Failed to encrypt form.'                                                         => 'Failed to encrypt form.',
-    'Encryption available.'                                                           => 'Encryption available.',
-    'Encryption not available.'                                                       => 'Encryption not available.',
+    'Encryption available.'                                                           => 'Form encryption is available.',
+    'Encryption not available.'                                                       => 'Form encryption is not available.',
 ];
